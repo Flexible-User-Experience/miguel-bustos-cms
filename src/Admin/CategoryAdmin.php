c97562c6180238e96983f8aded3327e74975b982
Albert Arques <aarques@infinitumdigital.net>
2025-04-30 12:43:20 +0200

    Add CategoryAdmin build

--- src/Admin/CategoryAdmin.php
+++ src/Admin/CategoryAdmin.php
@@ -6,14 +6,10 @@ use App\Enum\DoctrineEnum;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridInterface;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
-use Sonata\AdminBundle\FieldDescription\FieldDescriptionInterface;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Route\RouteCollectionInterface;
-use Sonata\DoctrineORMAdminBundle\Filter\ModelFilter;
-use Sonata\Form\Type\CollectionType;
-
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class CategoryAdmin extends AbstractAdmin
 {
@@ -29,6 +25,33 @@ final class CategoryAdmin extends AbstractAdmin
         $sortValues[DatagridInterface::SORT_BY] = 'name';
     }
 
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('name', TextType::class, [
+                'label' => 'Nombre',
+            ])
+        ;
+    }
 
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('name')
+        ;
+    }
 
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->addIdentifier('name')
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('name')
+        ;
+    }
 }
