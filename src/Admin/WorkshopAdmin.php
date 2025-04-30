bb7e433c0edaad6e2ceed29f684a67d5aceae490
Albert Arques <aarques@infinitumdigital.net>
2025-04-30 12:43:48 +0200

    Add WorkshopAdmin build

--- src/Admin/WorkshopAdmin.php
+++ src/Admin/WorkshopAdmin.php
@@ -2,13 +2,89 @@
 
 namespace App\Admin;
 
+use App\Enum\DoctrineEnum;
+use Doctrine\DBAL\Types\DateType;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridInterface;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class WorkshopAdmin extends AbstractAdmin
 {
     public function generateBaseRoutePattern(bool $isChildAdmin = false): string
     {
         return 'workshops/workshop';
+    }
+
+    protected function configureDefaultSortValues(array &$sortValues): void
+    {
+        $sortValues[DatagridInterface::PAGE] = 1;
+        $sortValues[DatagridInterface::SORT_ORDER] = DoctrineEnum::DESC->value;
+        $sortValues[DatagridInterface::SORT_BY] = 'startsAt';
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('title', TextType::class, [
+                'label' => 'Título',
+            ])
+            ->add('subtitle', TextType::class, [
+                'label' => 'Subtítulo',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Descripción',
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Precio',
+            ])
+            ->add('startsAt', DateType::class, [
+                'label' => 'Fecha de inicio',
+            ])
+            ->add('endsAt', DateType::class, [
+                'label' => 'Fecha de fin',
+            ])
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('title')
+            ->add('price')
+            ->add('startsAt')
+            ->add('endsAt')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->addIdentifier('title')
+            ->addIdentifier('subtitle')
+            ->addIdentifier('description')
+            ->addIdentifier('price')
+            ->addIdentifier('startsAt')
+            ->addIdentifier('endsAt')
+
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('title')
+            ->add('subtitle')
+            ->add('description')
+            ->add('price')
+            ->add('startsAt')
+            ->add('endsAt')
+        ;
     }
 
 }
