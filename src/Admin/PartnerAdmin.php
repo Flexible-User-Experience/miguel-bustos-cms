4b2ad35ae23e9535880e736d9454ccc05d0f3004
Albert Arques <aarques@infinitumdigital.net>
2025-04-30 12:43:29 +0200

    Add PartnerAdmin build

--- src/Admin/PartnerAdmin.php
+++ src/Admin/PartnerAdmin.php
@@ -2,7 +2,14 @@
 
 namespace App\Admin;
 
+use App\Enum\DoctrineEnum;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridInterface;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class PartnerAdmin extends AbstractAdmin
 {
@@ -11,4 +18,45 @@ final class PartnerAdmin extends AbstractAdmin
         return 'projects/partner';
     }
 
+    protected function configureDefaultSortValues(array &$sortValues): void
+    {
+        $sortValues[DatagridInterface::PAGE] = 1;
+        $sortValues[DatagridInterface::SORT_ORDER] = DoctrineEnum::ASC->value;
+        $sortValues[DatagridInterface::SORT_BY] = 'name';
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('name', TextType::class, [
+                'label' => 'Nombre',
+            ])
+            ->add('url', TextType::class, [
+                'label' => 'URL',
+            ])
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('name')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->addIdentifier('name')
+            ->addIdentifier('url')
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('name')
+            ->add('url')
+        ;
+    }
 }
