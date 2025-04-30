d5e3110dad6b8acd5a7380e1337906318ac93c0a
Albert Arques <aarques@infinitumdigital.net>
2025-04-30 12:43:38 +0200

    Add ProjectAdmin build

--- src/Admin/ProjectAdmin.php
+++ src/Admin/ProjectAdmin.php
@@ -2,7 +2,15 @@
 
 namespace App\Admin;
 
+use App\Enum\DoctrineEnum;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridInterface;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class ProjectAdmin extends AbstractAdmin
 {
@@ -11,4 +19,50 @@ final class ProjectAdmin extends AbstractAdmin
         return 'projects/project';
     }
 
+    protected function configureDefaultSortValues(array &$sortValues): void
+    {
+        $sortValues[DatagridInterface::PAGE] = 1;
+        $sortValues[DatagridInterface::SORT_ORDER] = DoctrineEnum::ASC->value;
+        $sortValues[DatagridInterface::SORT_BY] = 'name';
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('title', TextType::class, [
+                'label' => 'Título',
+            ])
+            ->add('subtitle', TextType::class, [
+                'label' => 'Subtítulo',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Descripción',
+            ])
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('title')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->addIdentifier('title')
+            ->addIdentifier('subtitle')
+            ->addIdentifier('description')
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('title')
+            ->add('subtitle')
+            ->add('description')
+        ;
+    }
 }
