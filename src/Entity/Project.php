a8ec6ca1a0fbcf16749fd59f5e573d3ba9e8e00d
Albert Arques <aarques@infinitumdigital.net>
2025-05-08 20:10:38 +0200

    Add Project > Category & Project > Partner relationships

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -3,17 +3,20 @@
 namespace App\Entity;
 
 use App\Entity\Trait\SlugTitleTrait;
+use App\Entity\Trait\TitleTrait;
 use App\Interface\SlugInterface;
 use App\Repository\ProjectRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
 class Project extends AbstractEntity implements SlugInterface
 {
     use SlugTitleTrait;
-
-    #[ORM\Column(length: 255)]
-    private ?string $title = null;
+    use TitleTrait;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $subtitle = null;
@@ -21,16 +24,18 @@ class Project extends AbstractEntity implements SlugInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $description = null;
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+    #[ORM\ManyToOne(inversedBy: 'projects')]
+    private ?Category $category = null;
 
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
+    /**
+     * @var Collection<int, Partner>
+     */
+    #[ORM\ManyToMany(targetEntity: Partner::class, inversedBy: 'projects')]
+    private Collection $partners;
 
-        return $this;
+    public function __construct()
+    {
+        $this->partners = new ArrayCollection();
     }
 
     public function getSubtitle(): ?string
@@ -53,6 +58,48 @@ class Project extends AbstractEntity implements SlugInterface
     public function setDescription(?string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Partner>
+     */
+    public function getPartners(): Collection
+    {
+        return $this->partners;
+    }
+
+    public function setPartners(Collection $partners): static
+    {
+        $this->partners = $partners;
+        return $this;
+    }
+
+    public function addPartner(Partner $partner): static
+    {
+        if (!$this->partners->contains($partner)) {
+            $this->partners->add($partner);
+        }
+
+        return $this;
+    }
+
+    public function removePartner(Partner $partner): static
+    {
+        $this->partners->removeElement($partner);
 
         return $this;
     }
