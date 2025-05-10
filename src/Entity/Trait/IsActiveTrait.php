255684cc84dfa7d6432e2924614fdff58f9c8424
Albert Arques <aarques@infinitumdigital.net>
2025-05-10 17:44:38 +0200

    Add IsActive Trait

---
+++ src/Entity/Trait/IsActiveTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity\Trait;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+trait IsActiveTrait
+{
+    #[ORM\Column(type: Types::BOOLEAN, nullable: false)]
+    protected bool $isActive = false;
+
+    public function getIsActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+        return $this;
+    }
+}
