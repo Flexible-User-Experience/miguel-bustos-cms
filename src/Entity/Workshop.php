022dde4491ea5349cf9a64135d3f9fb2c90c19eb
Albert Arques <aarques@infinitumdigital.net>
2025-05-16 14:33:12 +0200

    Add mainImageFile property

--- src/Entity/Workshop.php
+++ src/Entity/Workshop.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use App\Entity\Image\WorkshopImage;
 use App\Entity\Trait\ImagesTrait;
 use App\Entity\Trait\IsActiveTrait;
 use App\Entity\Trait\MainImageTrait;
@@ -11,12 +10,14 @@ use App\Entity\Trait\TitleTrait;
 use App\Interface\SlugInterface;
 use App\Repository\WorkshopRepository;
 use DateTimeInterface;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: WorkshopRepository::class)]
+#[Vich\Uploadable]
 class Workshop extends AbstractEntity implements SlugInterface
 {
     use IsActiveTrait;
@@ -31,6 +32,11 @@ class Workshop extends AbstractEntity implements SlugInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $description = null;
 
+    #[Assert\File(maxSize: '10M', extensions: ['png', 'jpg', 'jpeg'])]
+    #[Assert\Image(minWidth: 600)]
+    #[Vich\UploadableField(mapping: 'projects_photos', fileNameProperty: 'mainImage')]
+    private ?File $mainImageFile = null;
+
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     private ?DateTimeInterface $startsAt = null;
 
@@ -39,17 +45,6 @@ class Workshop extends AbstractEntity implements SlugInterface
 
     #[ORM\Column]
     private ?int $price = null;
-
-    /**
-     * @var Collection<int, WorkshopImage>
-     */
-    #[ORM\OneToMany(targetEntity: WorkshopImage::class, mappedBy: 'workshop')]
-    private Collection $images;
-
-    public function __construct()
-    {
-        $this->images = new ArrayCollection();
-    }
 
     public function getSubtitle(): ?string
     {
@@ -109,33 +104,8 @@ class Workshop extends AbstractEntity implements SlugInterface
         return $this;
     }
 
-    /**
-     * @return Collection<int, WorkshopImage>
-     */
-    public function getWorkshopImages(): Collection
+    public function __toString(): string
     {
-        return $this->images;
-    }
-
-    public function addWorkshopImage(WorkshopImage $workshopImage): static
-    {
-        if (!$this->images->contains($workshopImage)) {
-            $this->images->add($workshopImage);
-            $workshopImage->setWorkshop($this);
-        }
-
-        return $this;
-    }
-
-    public function removeWorkshopImage(WorkshopImage $workshopImage): static
-    {
-        if ($this->images->removeElement($workshopImage)) {
-            // set the owning side to null (unless already changed)
-            if ($workshopImage->getWorkshop() === $this) {
-                $workshopImage->setWorkshop(null);
-            }
-        }
-
-        return $this;
+        return $this->getTitle() ?: '';
     }
 }
