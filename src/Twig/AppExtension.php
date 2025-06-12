24c303a52e7865d3d82e4ac148a5e73bd97fa322
Albert Arques <aarques@infinitumdigital.net>
2025-06-12 18:58:13 +0200

    Add isInstanceOf Twig function

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigTest;
+
+class AppExtension extends AbstractExtension
+{
+    public function getTests(): array
+    {
+        return [
+            new TwigTest('instance_of', [$this, 'isInstanceOf']),
+        ];
+    }
+
+    public function isInstanceOf($var, $instance): bool
+    {
+        return new \ReflectionClass($instance)->isInstance($var);
+    }
+}
