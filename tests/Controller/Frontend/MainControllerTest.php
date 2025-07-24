6127d491f8862174d3875b7cd21a54899a01b9a5
David Romaní <david@flux.cat>
2025-07-24 18:34:50 +0200

    add some tests

---
+++ tests/Controller/Frontend/MainControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Controller\Frontend;
+
+use App\Enum\RoutesEnum;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $client->request(Request::METHOD_GET, RoutesEnum::app_frontend_homepage_index->value);
+        self::assertResponseIsSuccessful();
+        $client->request(Request::METHOD_GET, RoutesEnum::app_project_index->value);
+        self::assertResponseIsSuccessful();
+        $client->request(Request::METHOD_GET, RoutesEnum::app_workshop_index->value);
+        self::assertResponseIsSuccessful();
+    }
+}
