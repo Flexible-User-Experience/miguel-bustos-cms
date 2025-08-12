1f3114cf4cdcb5a4f9e4344408c8ef24d60b0462
David Romaní <david@flux.cat>
2025-08-12 19:24:51 +0200

    override ProjectAdminController showAction

--- src/Controller/Admin/ProjectAdminController.php
+++ src/Controller/Admin/ProjectAdminController.php
@@ -2,8 +2,17 @@
 
 namespace App\Controller\Admin;
 
+use App\Enum\RoutesEnum;
 use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProjectAdminController extends CRUDController
 {
+    public function showAction(Request $request): Response
+    {
+        return $this->redirectToRoute(RoutesEnum::app_project_detail->name, [
+            'slug' => $this->admin->getSubject()->getSlug(),
+        ]);
+    }
 }
