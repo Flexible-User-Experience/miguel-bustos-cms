4dd247d66923da4a721435fec6c503acaed5cfe4
David Romaní <david@flux.cat>
2025-08-19 12:12:41 +0200

    add isGifFileFormat Twig test extension

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -11,11 +11,29 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigTest('instance_of', $this->isInstanceOf(...)),
+            new TwigTest('is_gif_file_format', $this->isGifFileFormat(...)),
         ];
     }
 
     public function isInstanceOf($var, $instance): bool
     {
         return new \ReflectionClass($instance)->isInstance($var);
+    }
+
+    public function isGifFileFormat(string $filename): bool
+    {
+        // 1. Fast check: by file extension
+        if ('gif' === strtolower(pathinfo($filename, PATHINFO_EXTENSION))) {
+            return true;
+        }
+
+        // 2. (Optional) Safer check: by mime type if file exists
+        if (is_file($filename)) {
+            $mime = mime_content_type($filename);
+
+            return 'image/gif' === $mime;
+        }
+
+        return false;
     }
 }
