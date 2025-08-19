57b6462e554b87ae0d4a955d32b1813bd61a3777
David Romaní <david@flux.cat>
2025-08-19 17:04:47 +0200

    add more translations

--- src/Controller/Frontend/HomepageController.php
+++ src/Controller/Frontend/HomepageController.php
@@ -6,10 +6,13 @@ use App\Entity\ContactMessage;
 use App\Enum\LocaleEnum;
 use App\Enum\RoutesEnum;
 use App\Form\Type\ContactMessageFormType;
+use App\Kernel;
+use App\Repository\ContactMessageRepository;
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomepageController extends AbstractController
@@ -33,15 +36,22 @@ class HomepageController extends AbstractController
             LocaleEnum::ca => RoutesEnum::app_contact_ca->value,
         ],
         name: RoutesEnum::app_contact->name,
-        methods: [Request::METHOD_GET]
+        methods: [Request::METHOD_GET, Request::METHOD_POST]
     )]
-    public function contact(Request $request): Response
+    public function contact(Request $request, ContactMessageRepository $cmr, KernelInterface $kernel): Response
     {
         $contactMessage = new ContactMessage();
         $form = $this->createForm(ContactMessageFormType::class, $contactMessage);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->addFlash('success', 'Your message has been sent successfully!');
+            if (Kernel::DEV_ENV === $kernel->getEnvironment()) { // TODO remove this when in production
+                $cmr->add(
+                    entity: $contactMessage,
+                    flush: true
+                );
+                // TODO send email to admin
+                $this->addFlash('success', 'Your message has been sent successfully!');
+            }
 
             return $this->redirectToRoute(RoutesEnum::app_contact->name, [], Response::HTTP_SEE_OTHER);
         }
