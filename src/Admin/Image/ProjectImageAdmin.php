289e16e3727b36ff3105ae205430ee9aa4a673d6
David Romaní <david@flux.cat>
2025-11-07 18:21:40 +0100

    add default sort and filters to ProjectImageAdmin; implement custom query builder in ProjectRepository

--- src/Admin/Image/ProjectImageAdmin.php
+++ src/Admin/Image/ProjectImageAdmin.php
@@ -3,8 +3,13 @@
 namespace App\Admin\Image;
 
 use App\Admin\AbstractBaseAdmin;
+use App\Entity\Category;
 use App\Entity\Project;
+use App\Enum\DoctrineEnum;
+use Sonata\AdminBundle\Datagrid\DatagridInterface;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\DoctrineORMAdminBundle\Filter\ModelFilter;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -15,6 +20,13 @@ class ProjectImageAdmin extends AbstractBaseAdmin
     public function generateBaseRoutePattern(bool $isChildAdmin = false): string
     {
         return 'projects/image';
+    }
+
+    protected function configureDefaultSortValues(array &$sortValues): void
+    {
+        $sortValues[DatagridInterface::PAGE] = 1;
+        $sortValues[DatagridInterface::SORT_ORDER] = DoctrineEnum::ASC->value;
+        $sortValues[DatagridInterface::SORT_BY] = 'project.position';
     }
 
     protected function configureFormFields(FormMapper $form): void
@@ -52,6 +64,23 @@ class ProjectImageAdmin extends AbstractBaseAdmin
                 NumberType::class,
                 [
                     'required' => true,
+                ]
+            )
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add(
+                'project',
+                ModelFilter::class,
+                [
+                    'field_options' => [
+                        'class' => Project::class,
+                        'choice_label' => 'title',
+                        'query_builder' => $this->getEntityManager()->getRepository(Project::class)->findAllSortedByPositionAndTitleQB(),
+                    ],
                 ]
             )
         ;
